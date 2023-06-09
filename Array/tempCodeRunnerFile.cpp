6929cbb5b6689994d57f27a384fe9dbe7508c03c
sakhasib <adibsakhawat@iut-dhaka.edu>
2023-06-10 02:16:51 +0600

    md updated

--- Array/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-    int n = sizeof(arr)/sizeof(int);
-    int key;
-    cout << "what you wanna find ??   :  ";
-    cin >> key;
-    int index = Binary_Search(arr, n, key);
